d74b83fa719f65a965293a05c26ee8ad4da356b1
KhuyenNH <huukhuyen.dev@gmail.com>
2018-03-20 16:05:24 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 /*  Scroll play video youtube
 --------------------------------------*/
-
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -14,16 +13,33 @@ function onYouTubePlayerAPIReady() {
         playerVars: {
             autoplay: 0
         },
-        videoId: 'bDFXTXeA6ow'
+        videoId: 'qu9TiWocgTA'
     });
 }
-
 $(window).scroll(function() {
+    var scroll = $(this).scrollTop();
     $("iframe").each(function() {
-        if ($(window).scrollTop() > $(this).offset().top - 400) {
+        if (scroll > $(this).offset().top - 400) {
             player.playVideo();
         } else {
             player.stopVideo();
         }
     });
+    var posCircle = $('.local_circle').offset().top;
+    if(scroll > posCircle - 400) {
+        $('.circle_01').addClass('show');
+        setTimeout(function () {
+            $('.circle_02').addClass('show');
+        }, 400);
+    }
+});
+
+/* Scrool bookmark
+-------------------------------------- */
+$('a[href^="#"]').click(function() {
+    var the_id = $(this).attr("href");
+    $('html, body').animate({
+        scrollTop: $(the_id).offset().top
+    }, 1000);
+    return false;
 });
